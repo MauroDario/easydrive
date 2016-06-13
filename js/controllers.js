141e8996d6a1f8dc89e9d35d0c88a82dc8ff3a9f
Pablo Corrado <corrado.pablom@gmail.com>
2016-06-12 22:13:41 -0300

    Cambios varios

--- js/controllers.js
+++ js/controllers.js
@@ -1,58 +1,124 @@
-angular.module('app.controllers', [])
-  
-.controller('homeCtrl', function($scope) {
+angular.module('app.controllers', ['app.service'])
+
+.controller('homeCtrl', function ($scope, $rootScope, translationService, $ionicPopup) {
+
+    // Inicializo Combo de Lenguajes
+    $scope.data = {
+        ddlLanguage: "es_ES"
+    }
+
+    // Defino función de cambio de Lenguaje
+    $rootScope.translate = function (language) {
+        $rootScope.selectedLanguage = language;
+        translationService.getTranslation($rootScope, $rootScope.selectedLanguage);
+    };
+
+    // Idioma default Español --> ToDo!
+    $rootScope.translate("es_ES");
+
+    // Cambio lenguaje cuando modifico el valor del Combo de Lenguajes
+    $scope.changeLanguage = function () {
+        $rootScope.translate($scope.data.ddlLanguage);
+    }
+
+
+    $scope.showAlert = function () {
+        var alertPopup = $ionicPopup.alert({
+            title: 'Don\'t eat that!',
+            template: 'It might taste good'
+        });
+
+        alertPopup.then(function (res) {
+            console.log('Thank you for not eating my delicious ice cream cone');
+        });
+    };
 
 })
-   
-.controller('oilChangeCtrl', function($scope) {
+
+.controller('oilChangeCtrl', function ($scope, sqlService) {
+    sqlService.select("oilChangeCtrl").then(function (data) {
+        $scope.foo = {
+            days: 0
+        };
+        if (data.rows.length > 0) {
+            $scope.foo.days = data.rows.item(0).day_value;
+            console.log("se obtuvo " + $scope.foo.days);
+        } else {
+            console.log("else");
+            $scope.foo.days = 15;
+        }
+    }, function (err) {
+        console.log(err);
+    });
+
+    $scope.update = function () {
+        console.log("updateto: " + $scope.foo.days);
+        sqlService.insertOrUpdate("oilChange", $scope.foo.days);
+    };
+})
+
+.controller('oilFilterChangeCtrl', function ($scope) {
 
 })
-   
-.controller('oilFilterChangeCtrl', function($scope) {
+
+.controller('airFilterChangeCtrl', function ($scope, sqlService) {
+    sqlService.select("oilChangeCtrl").then(function (data) {
+        $scope.daysInput = {
+            value: 0
+        };
+        
+        if (data.rows.length > 0) {
+            $scope.daysInput.value = data.rows.item(0).day_value;
+            console.log("se obtuvo " + $scope.daysInput.value);
+        } else {
+            console.log("else");
+            $scope.daysInput.value = 100;
+        }
+    }, function (err) {
+        console.log(err);
+    });
+
+    $scope.save = function () {
+        sqlService.insertOrUpdate("airFilterChange", $scope.daysInput.value);
+    }
+})
+
+.controller('cabinFilterChangeCtrl', function ($scope) {
 
 })
-   
-.controller('airFilterChangeCtrl', function($scope) {
+
+.controller('plugsChangeCtrl', function ($scope) {
 
 })
-   
-.controller('cabinFilterChangeCtrl', function($scope) {
+
+.controller('tiresChangeCtrl', function ($scope) {
 
 })
-   
-.controller('plugsChangeCtrl', function($scope) {
+
+.controller('dampersChangeCtrl', function ($scope) {
 
 })
-   
-.controller('tiresChangeCtrl', function($scope) {
+
+.controller('carWorkshopReviewCtrl', function ($scope) {
 
 })
-   
-.controller('dampersChangeCtrl', function($scope) {
+
+.controller('coolantchangeCtrl', function ($scope) {
 
 })
-   
-.controller('carWorkshopReviewCtrl', function($scope) {
+
+.controller('brakepadschangeCtrl', function ($scope) {
 
 })
-   
-.controller('coolantchangeCtrl', function($scope) {
+
+.controller('brakefluidchangeCtrl', function ($scope) {
 
 })
-   
-.controller('brakepadschangeCtrl', function($scope) {
+
+.controller('timingbeltchangeCtrl', function ($scope) {
 
 })
-   
-.controller('brakefluidchangeCtrl', function($scope) {
+
+.controller('oilFilterChange2Ctrl', function ($scope) {
 
 })
-   
-.controller('timingbeltchangeCtrl', function($scope) {
-
-})
-   
-.controller('oilFilterChange2Ctrl', function($scope) {
-
-})
-    
--- js/service.js
+++ js/service.js
@@ -2,17 +2,112 @@ angular.module('app.service', ['ngResource'])
 
 .service('translationService', function ($resource) {
     this.getTranslation = function ($rootScope, language) {
-      var languageFilePath = 'Resource/i18n/translation_' + language + '.json';
-      $resource(languageFilePath).get(function (data) {
-        $rootScope.translation = data;
+        var languageFilePath = 'Resource/i18n/translation_' + language + '.json';
+        $resource(languageFilePath).get(function (data) {
+            $rootScope.translation = data;
 
-      });
+        });
     };
-  })
-.service("sqlService", function ($cordovaSQLite) {
-  this.execute = function (query, parameter) {
-    if (parameter == null)
-      parameter = [];
-    return $cordovaSQLite.execute(db, query, parameter);
-  };
+})
+
+.service("sqlService", function ($cordovaSQLite, $ionicPopup) {
+    var self = this;
+    self.execute = function (query, parameter) {
+        if (parameter == null)
+            parameter = [];
+        return $cordovaSQLite.execute(db, query, parameter);
+    };
+
+    self.insert = function (id, day_value) {
+        var query = "INSERT INTO abm_values (id, day_value) VALUES (?,?)";
+        $cordovaSQLite.execute(db, query, [id, day_value]).then(function (res) {
+            console.log("INSERT ID -> " + res.insertId);
+            
+            var alertPopup = $ionicPopup.alert({
+                title: 'Guardado exitoso!',
+                template: 'El valor fue guardado exitosamente.'
+            });
+
+            alertPopup.then(function (res) {
+                // ToDo!
+            });
+            
+        }, function (err) {
+            console.error(err);
+            
+            var alertPopup = $ionicPopup.alert({
+                title: 'Error!',
+                template: 'No se ha podido completar la solicitud.'
+            });
+
+            alertPopup.then(function (res) {
+                // ToDo!
+            });
+            
+        });
+    };
+
+    self.select = function (id) {
+        var query = "SELECT id, day_value FROM abm_values WHERE id = ?";
+        return $cordovaSQLite.execute(db, query, [id]);
+
+    };
+
+    self.deleteAll = function () {
+        var query = "DELETE FROM abm_values";
+        $cordovaSQLite.execute(db, query).then(function (res) {
+            console.log("DELETE ALL");
+        }, function (err) {
+            console.error(err);
+        });
+    };
+
+    self.delete = function (id) {
+        var query = "DELETE FROM abm_values WHERE id = ?";
+        $cordovaSQLite.execute(db, query, [id]).then(function (res) {
+            console.log("DELETE ALL");
+        }, function (err) {
+            console.error(err);
+        });
+    };
+
+
+    self.update = function (day_value, id) {
+        var query = "UPDATE abm_values SET day_value = ? WHERE id = ?";
+        $cordovaSQLite.execute(db, query, [day_value, id]).then(function (res) {
+            console.log("UPDATE");
+            
+            var alertPopup = $ionicPopup.alert({
+                title: 'Guardado exitoso!',
+                template: 'El valor fue guardado exitosamente.'
+            });
+
+            alertPopup.then(function (res) {
+                // ToDo!
+            });
+            
+        }, function (err) {
+            console.error(err);
+            
+            var alertPopup = $ionicPopup.alert({
+                title: 'Error!',
+                template: 'No se ha podido completar la solicitud.'
+            });
+
+            alertPopup.then(function (res) {
+                // ToDo!
+            });
+            
+        });
+    };
+
+    self.insertOrUpdate = function (id, day_value) {
+        this.select(id).then(function (data) {
+            if (data.rows.length > 0) {
+                self.update(day_value, id);
+            } else {
+                self.insert(id, day_value);
+            }
+        });
+    };
 })
